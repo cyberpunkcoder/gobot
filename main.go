e868e293d3198b0d61a00a5c8890fa5b2dd644f1
cyberpunkcoder <cyberpunkcoder@gmail.com>
2020-05-04 00:06:27 -0700

    Events added and working
    Bot informs the user of its username when logged in.
    This is to confirm to the user which bot has been logged into on discord.
    
    Bot replies "ping" and "pong" respectively when detected as a chat message.

--- main.go
+++ main.go
@@ -2,22 +2,74 @@ package main
 
 import (
 	"fmt"
+	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 	"os"
-	//"github.com/bwmarrin/discordgo"
+	"os/signal"
+	"syscall"
 )
 
-func init() {
+func main() {
 	err := godotenv.Load()
 
-	//Check if
+	// Check if .env file exists
 	if err != nil {
-		fmt.Println("Error loading .env file")
+		fmt.Println("Error loading .env file, ", err)
+		return
 	}
 
-	fmt.Println(os.Getenv("DISCORDTOKEN"))
+	discord, err := discordgo.New("Bot " + os.Getenv("DISCORDTOKEN"))
+
+	// Check if Discord session was created
+	if err != nil {
+		fmt.Println("Error creating Discord session,", err)
+		return
+	}
+
+	discord.AddHandler(ready)
+	discord.AddHandler(messageCreate)
+
+	err = discord.Open()
+
+	// Check if Discord connection was made
+	if err != nil {
+		fmt.Println("Error opening Discord connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session
+	discord.Close()
 }
 
-func main() {
+/* This function will be called (due to AddHandler above) every time the bot
+sucessfully logs in. */
+func ready(s *discordgo.Session, r *discordgo.Ready) {
 
+	// Display name of bot to user after login successful
+	fmt.Println("Logged in as", s.State.User, "Press CTRL-C to exit")
+}
+
+/* This function will be called (due to AddHandler above) every time a new
+message is created on any channel that the autenticated bot has access to. */
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 }
