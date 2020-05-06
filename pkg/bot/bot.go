332f94bb5d501a6504246d7dd858632728459592
cyberpunkprogrammer <cyberpunkprogrammer@gmail.com>
2020-05-05 23:50:28 -0700

    Organized commands into folder
    Folder "pkg/bot/command/" contains all of the bot commands.
    Command "hello" was added as a test of this structure.
    Bot checks if command is prefixed with the CommandPrefix stored in the config.json file.
    CommandPrefix is completely configurable, but is set to "!" as an example.

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -4,26 +4,30 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/cyberpunkprogrammer/gobot/pkg/bot/command"
 	"github.com/cyberpunkprogrammer/gobot/pkg/config"
 )
 
-func Run() {
+// Start the Discord bot
+func Start() {
 
-	discord, err := discordgo.New("Bot " + config.DiscordToken)
+	// Create new bot session
+	botSession, err := discordgo.New("Bot " + config.DiscordToken)
 
-	// Check if Discord session was created
+	// Check if bot was created
 	if err != nil {
 		fmt.Println("Error creating Discord session,", err)
 		return
 	}
 
-	discord.AddHandler(ready)
-	discord.AddHandler(messageCreate)
+	botSession.AddHandler(ready)
+	botSession.AddHandler(messageCreate)
 
-	err = discord.Open()
+	err = botSession.Open()
 
 	// Check if Discord connection was made
 	if err != nil {
@@ -31,37 +35,37 @@ func Run() {
 		return
 	}
 
-	// Wait here until CTRL-C or other term signal is received
+	// Run until CTRL-C or other term signal is received
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
 	// Cleanly close down the Discord session
-	discord.Close()
+	botSession.Close()
 }
 
-// This function will be called (due to AddHandler above) every time the bot sucessfully logs in
-func ready(s *discordgo.Session, r *discordgo.Ready) {
+// ready is called whenever the bot has successfully logged in
+func ready(session *discordgo.Session, ready *discordgo.Ready) {
 
-	// Display name of bot to user after login successful
-	fmt.Println("Logged in as", s.State.User, "Press CTRL-C to exit")
+	// Display name of bot to user
+	fmt.Println("Logged in as", session.State.User, "Press CTRL-C to exit")
 }
 
-// This function will be called (due to AddHandler above) every time a new message is created on any channel that the autenticated bot has access to
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+// messageCreate is called whenever a message has been created
+func messageCreate(session *discordgo.Session, message *discordgo.MessageCreate) {
 
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
+	if strings.HasPrefix(message.Content, config.CommandPrefix) {
 
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
+		commandText := message.Content[1:]
+		fmt.Println(commandText)
+
+		// Ignore all messages created by the bot itself
+		if message.Author.ID == session.State.User.ID {
+			return
+		}
+
+		if commandText == "hello" {
+			command.Hello(session, message)
+		}
 	}
 }
