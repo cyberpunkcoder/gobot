dd33c62a86357386d7c25f7a93d7bee708e543de
cyberpunkcoder <cyberpunkcoder@gmail.com>
2020-08-01 11:49:58 -0700

    Added configuration prompt

--- pkg/bot/config/config.go
+++ pkg/bot/config/config.go
@@ -1,9 +1,13 @@
 package config
 
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 )
 
 var (
@@ -16,37 +20,122 @@ var (
 	// JoinRole role members are given after joining guild
 	JoinRole string
 
-	// Private variables
-	config *configStruct
+	// configFile is the file which contians the program startup configuration
+	configFile = "../../json/config.json"
 )
 
 type configStruct struct {
-	DiscordToken  string `json:"DiscordToken"`
-	CommandPrefix string `json:"CommandPrefix"`
-	JoinRole      string `json:"JoinRole"`
+	DiscordToken  string
+	CommandPrefix string
+	JoinRole      string
 }
 
 // LoadConfig read and load the config.json file
 func LoadConfig() error {
 	log.Println("Loading config")
 
-	file, err := ioutil.ReadFile("../../json/config.json")
+	file, err := ioutil.ReadFile(configFile)
 
+	// Return if there was a problem reading the file
 	if err != nil {
-		log.Println(err.Error())
 		return err
 	}
 
-	err = json.Unmarshal(file, &config)
+	load := configStruct{}
+
+	err = json.Unmarshal(file, &load)
 
 	if err != nil {
-		log.Println(err.Error())
 		return err
 	}
 
-	DiscordToken = config.DiscordToken
-	CommandPrefix = config.CommandPrefix
-	JoinRole = config.JoinRole
+	DiscordToken = load.DiscordToken
+	CommandPrefix = load.CommandPrefix
+	JoinRole = load.JoinRole
 
 	return nil
+}
+
+// SaveConfig saves the configuration to drive from memory
+func SaveConfig() error {
+
+	save := configStruct{
+		DiscordToken:  DiscordToken,
+		CommandPrefix: CommandPrefix,
+		JoinRole:      JoinRole,
+	}
+
+	fmt.Println(save)
+
+	configJSON, err := json.MarshalIndent(save, "", " ")
+
+	fmt.Println(configJSON)
+
+	// Return if there was a problem marshaling config
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(configFile, configJSON, 0644)
+
+	// Return if there was an error writing to the file
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Create creates a new configuration by prompting the user
+func Create() {
+	fmt.Print("Create new configuration? (y/n): ")
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		response, err := reader.ReadString('\n')
+
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		if response == "y\n" {
+
+			// Obtain bot token from user and store into variable
+			fmt.Print("Enter discord bot token: ")
+			discordToken, err := reader.ReadString('\n')
+			DiscordToken = strings.TrimRight(discordToken, "\n")
+
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			// Obtain command prefix from user and store into variable
+			fmt.Print("Enter command prefix (For example '!'): ")
+			commandPrefix, err := reader.ReadString('\n')
+			CommandPrefix = strings.TrimRight(commandPrefix, "\n")
+
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			// Obtain role id from user and store into variable
+			fmt.Print("Enter role ID users get after joining (blank for none): ")
+			joinRole, err := reader.ReadString('\n')
+			JoinRole = strings.TrimRight(joinRole, "\n")
+
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			SaveConfig()
+			return
+		}
+
+		if response == "n\n" {
+			return
+		}
+	}
 }
