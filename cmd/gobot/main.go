89c996fa4b38ed46de7cc96d1c7cd3612a5cf56e
cyberpunkprogrammer <cyberpunkprogrammer@gmail.com>
2020-08-01 14:59:10 -0700

    Changed program file structure

--- cmd/gobot/main.go
+++ cmd/gobot/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/cyberpunkprogrammer/gobot/pkg/bot"
@@ -10,14 +12,30 @@ import (
 	"github.com/cyberpunkprogrammer/gobot/pkg/bot/reactionrole"
 )
 
+// BinPath is the location of the executable binary
+var BinPath string
+
 func main() {
+	BinPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Create directory nstructure if it does not already exist
+	_ = os.MkdirAll(BinPath+"/json/role", 0777)
+
+	// Set relative path to packages that access json files
+	config.BinPath = BinPath
+	reactionrole.BinPath = BinPath
 
 	// Load configuration for bot and api
-	err := config.LoadConfig()
+	err = config.LoadConfig()
 
 	// Return if error loading config.json
 	if err != nil {
-		if strings.Contains(err.Error(), "no such file or directory") {
+		if strings.Contains(err.Error(), "no") && strings.Contains(err.Error(), "directory") {
 			fmt.Println("Configuration not found")
 			config.Create()
 		} else {
